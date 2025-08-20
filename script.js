fe56625af82010aa84dc921c9a6a5e538aaf41bd
atshaw1994 <atshaw1994@gmail.com>
2025-08-20 13:13:44 -0400

    Reorder styling and redesign "No results found" message.

--- script.js
+++ script.js
@@ -3,18 +3,17 @@ const new_btn = document.querySelector('#btn-new');
 const upload_btn = document.querySelector('#btn-upload');
 const share_btn = document.querySelector('#btn-share');
 
+const contentContainer = document.querySelector('.content-container');
+
+const cards = document.querySelectorAll('.card');
 const card_btns = document.querySelectorAll('.card-buttons');
 const visibility_btns = document.querySelectorAll('.card-btn.material-icons-outlined:nth-child(2)');
 const favorite_btns = document.querySelectorAll('.card-btn.material-icons-outlined:nth-child(1)');
 
-const announcement_cards = document.querySelectorAll('.announcement-card');
-const project_cards = document.querySelectorAll('.project-card');
-
 const searchbar = document.querySelector('#searchbar');
 const searchbar_clear_icon = document.querySelector('.searchbar-clear');
 
-const msg_no_projects = document.querySelector('#no-projects-message');
-const msg_no_announcements = document.querySelector('#no-announcements-message');
+const noResultsMsg = document.querySelector('#no-results-message');
 
 let notificationsOn = true;
 
@@ -29,47 +28,38 @@ notificationBtn.addEventListener('click', () => {
 
 visibility_btns.forEach(icon => {
     icon.addEventListener('click', () => {
-        const card = icon.closest('.project-card');
+        const card = icon.closest('.card');
         // Toggle unread state
         card.dataset.unread = card.dataset.unread === "true" ? "false" : "true";
         if (card.dataset.unread == "true") {
-            card.classList.add("project-card-unread");
+            card.classList.add("card-unread");
         }
         else {
-            card.classList.remove("project-card-unread");
+            card.classList.remove("card-unread");
         }
     });
 });
 
 favorite_btns.forEach(icon => {
     icon.addEventListener('click', () => {
-        const card = icon.closest('.project-card');
+        const card = icon.closest('.card');
         // Toggle isFavorite state
         card.dataset.isFavorite = card.dataset.isFavorite === "true" ? "false" : "true";
         if (card.dataset.isFavorite == "true") {
-            card.classList.add("project-card-favorite");
+            card.classList.add("card-favorite");
             icon.style.color = "#ffcc00"
         }
         else {
-            card.classList.remove("project-card-favorite");
+            card.classList.remove("card-favorite");
             icon.style.color = "black"
         }
     });
 });
 
-project_cards.forEach(card => {
+cards.forEach(card => {
     card.addEventListener('click', (e) => {
         if (!e.target.closest('.card-btn') && card.dataset.unread == "true") {
-            card.classList.remove("project-card-unread");
-            card.dataset.unread = "false";
-        }
-    });
-});
-
-announcement_cards.forEach(card => {
-    card.addEventListener('click', () => {
-        if (card.dataset.unread == "true") {
-            card.classList.remove("announcement-card-unread");
+            card.classList.remove("card-unread");
             card.dataset.unread = "false";
         }
     });
@@ -83,7 +73,7 @@ function toggleClearIcon() {
     }
 }
 
-function searchForCard(cards, searchTerm) {
+function searchForCard(searchTerm) {
     let anyVisible = false;
     cards.forEach(card => {
         const card_content = card.querySelector('.card-content').textContent.toLowerCase();
@@ -100,10 +90,18 @@ function searchForCard(cards, searchTerm) {
 searchbar.addEventListener('input', () => {
     const searchTerm = searchbar.value.toLowerCase();
     toggleClearIcon();
-    // Projects
-    msg_no_projects.style.opacity = searchForCard(project_cards, searchTerm) ? '0' : '1';
-    // Announcements
-    msg_no_announcements.style.opacity = searchForCard(announcement_cards, searchTerm) ? '0' : '1';
+    let cardFound = searchForCard(searchTerm);
+    if (!cardFound) {
+        Array.from(contentContainer.children).forEach(child => {
+            child.style.display = 'none';
+        });
+        noResultsMsg.style.opacity = '1';
+    } else {
+        Array.from(contentContainer.children).forEach(child => {
+            child.style.display = '';
+        });
+        noResultsMsg.style.opacity = '0';
+    }
 });
 
 searchbar_clear_icon.addEventListener('click', () => {
